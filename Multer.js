aa779ef5d202e36e84cba93539108fff882afcf8
SHIBU-S <shibusahadevan018@gmail.com>
2024-11-28 16:28:57 +0530

    Backend

--- Multer.js
+++ Multer.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 const multer = require("multer");
@@ -5,30 +6,57 @@ const path = require("path");
 
 const port = 4444;
 
-app.use(express.static(__dirname));
-
-storage_field = multer.diskStorage(
-    {
-        destination : (req,file,cb)=>{
-            cb(null,path.join(__dirname,"Images"));
-        },
-
-        filename : (req,file,cb)=>{
-            cb(null,file.filename + Date.now() + file.originalname)
-        }
-    }
-)
-
-const upload = multer({storage : storage_field});
-
-app.post("/Images",upload.single("image"),(req,res)=>{
-    if(req.url==="/Images")
-    {
-        res.send("File successfully uploaded..");
+const storage_field = multer.diskStorage({
+    destination : (req,file,cb)=>{
+        cb(null, path.join(__dirname,"Images"));
+    },
+    filename : (req,file,cb)=>{
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
-app.listen(port,()=>{
-    console.log(`Server is listening on port ${port}`)
+function fileFilter_field(req,file,cb){
+    const filetype = ["image/jpg","image/jpeg","image/png"];
+    if(filetype.includes(file.mimetype))
+    {
+        cb(null,true);
+    }
+    else
+    {
+        cb(new Error("It's not allowed to upload"),false);
+    }
+}
+
+const upload = multer({
+    storage : storage_field,
+    fileFilter : fileFilter_field
+});
+
+
+app.post("/Image",upload.single("imageupload"),(req,res,next)=>{
+    if(req.file)
+    {
+        res.send(`File successfully uploaded..`);
+    }
+    else
+    {
+        res.send("No File uploaded..");
+    }
+});
+
+app.use((err,req,res,next)=>{
+    if(err.message === "It's not allowed to upload")
+    {
+        res.send("It's not allowed to upload");
+    }
+    else
+    {
+        res.send("Someting went wrong");
+    }
 })
+
+app.listen(port,()=>{
+    console.log(`Server is listening on the port ${port}`)
+});
+
 
